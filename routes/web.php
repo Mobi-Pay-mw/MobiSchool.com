38e09047caec536eea057b8c33300d8f89d4f017
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-10-07 16:18:50 +0200

    main.2.0

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,19 @@
 use App\Http\Controllers\RepositoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\userauthcontroller;
-<<<<<<< HEAD
-use App\Http\Controllers\sessioncontroller;
-use App\Http\Controllers\RepositoryController;
 
 
 
-route::get('/regform',[registrationcontroller::class,'create']);
-route::post('/reg',[registrationcontroller::class,'store']);
-route::post('/login',[userauthcontroller::class,'login'] );
-route::get('/',[sessioncontroller::class,'create']);
-route::get('/welcome', function(){
+// route::get('/regform',[registrationcontroller::class,'create']);
+// route::post('/reg',[registrationcontroller::class,'store']);
+// route::post('/login',[userauthcontroller::class,'login'] );
+// route::get('/',[sessioncontroller::class,'create']);
+// route::get('/welcome', function(){
 
-    return view('welcome'); 
-});
-=======
->>>>>>> parent of badaa0c (Merge branch 'Assignment' into Assessment_2.0)
+//     return view('welcome'); 
+// });
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -62,17 +59,6 @@ Route::get('/about', function(){
     return view('pages.about');
 });
 
-<<<<<<< HEAD
-// Route::get('/sign_in', function(){
-//     return view('login');
-// });
-
-// Route::get('/sign_up', function(){
-//     return view('registration');
-// });
-
-=======
->>>>>>> parent of badaa0c (Merge branch 'Assignment' into Assessment_2.0)
 
 Route::get('/stdashboard', function(){
     return view('pages.stdashboard');
@@ -85,7 +71,6 @@ Route::get('/thdashboard', function(){
 Route::get('/homepage', function(){
     return view('pages.homepage');
 });
-<<<<<<< HEAD
 
 Route::get('/login',function()
 {
@@ -100,11 +85,8 @@ Route::get('/reg',function()
 });
 
 // login 
-Route::post('/session', [userauthcontroller::class, 'login']);
 
 Route::get('asses', [AssesmentController::class, 'index']);
-=======
->>>>>>> parent of badaa0c (Merge branch 'Assignment' into Assessment_2.0)
 
 Route::get('/login',function()
 {
@@ -122,7 +104,6 @@ Route::post('/session', [userauthcontroller::class, 'login']);
 
 Route::post('/upload_content', [RepositoryController::class, 'contentStore']);
 
-<<<<<<< HEAD
 Route::put('/assesupdate/{id}', [AssesmentController::class, 'update']);
 
 Route::get('/assesdelete/{id}', [AssesmentController::class, 'destroy']);
@@ -138,11 +119,7 @@ Route::put('/queupdate/{id}', [QuestionController::class, 'update']);
 Route::get('/quedelete/{id}', [QuestionController::class, 'destroy']);
 
 Route::get('/exam', [ExamController::class, 'index']);
-Route::get('/reg',function()
-{
-    # code...
-    return view('registration'); 
-});
+
 Route::post('/upload_content', [RepositoryController::class, 'contentStore']);
 
 Route::get('/quizshow/{id}', [ExamController::class, 'show']);
@@ -150,5 +127,4 @@ Route::get('/quizshow/{id}', [ExamController::class, 'show']);
 Route::post('/quizstore', [ExamController::class, 'store']);
 
 Route::get('/result', [ExamController::class, 'results']);
-=======
->>>>>>> parent of badaa0c (Merge branch 'Assignment' into Assessment_2.0)
+
